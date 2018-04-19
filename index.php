004196fb81a47e1a385b7660f4634fe6b42f32df
joshalva <37341386+joshalva@users.noreply.github.com>
2018-04-19 13:04:11 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
+	<?php include('server.php') ?>
 <?php
-try {
-    $hostname = "localhost";
-    $dbname = "alpha";
-    $user = "root";
-    $dbalpha = new PDO("mysql:host=$hostname;dbname=$dbname", $user);
-} catch (PDOException $e) {
-    echo "Errore: " . $e->getMessage();
-    die();
-}
+//try {
+//    $hostname = "localhost";
+//    $dbname = "alpha";
+//    $user = "root";
+//    $dbalpha = new PDO("mysql:host=$hostname;dbname=$dbname", $user);
+//} catch (PDOException $e) {
+////    echo "Errore: " . $e->getMessage();
+////    die();
+//}
 
-session_start();
+//session_start();
 
 if (!isset($_SESSION['nickname'])) {
     $_SESSION['msg'] = "Devi effettuare il login";
@@ -26,28 +27,44 @@ if (isset($_GET['logout'])) {
 <html>
     <head>
         <title>SeeMyCity - Home</title>
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="style.css">    
+        <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.1/themes/base/minified/jquery-ui.min.css">
+         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <script type="text/javascript" src="http://code.jquery.com/ui/1.10.1/jquery-ui.min.js"></script> 
     </head>
     <body>
-        <div class="navcont">
-            <nav id="navbar">
-                <ul>
-                    <li id="nav1"><a href="index.php"><img src="images/logohome.PNG" alt="logo" width="100px" height="50px"></a></li>
-                    <li id="nav2"><em>Benvenuto<strong> <?php echo $_SESSION['nickname']; ?></strong>!</em></li>
-                    <li id="nav3"><img id="userph" src="images/user.png" width="45px" height="45px" ></li>
-                </ul>
-            </nav>
-        </div>
-        <div class="contar">  <h1 id="titlehome"><em>Choose your destination</em></h1>
-        </div>
-        <br>
-        
-        <form action="/action_page.php" method ='POST'>City name!<br>
-            <input type="text" name="nomecitta">
-        </form>
-       
         <div class="grid-container">
-              <div class="grid-item sideCont">   
+            <div class="navcont">
+                <nav id="navbar">
+                    <ul>
+                        <li id="nav1"><a href="index.php"><img src="images/logohome.PNG" alt="logo" width="100px" height="50px"></a></li>
+                        <li id="nav2"><em>Benvenuto<strong> <?ph        p echo $_SESSION['nickname']; ?></strong>!</em></li>
+                        <li id="nav3"><div class="dropdown">
+                              <button onclick="myFunction()" class="dropbtn"></button>
+                                  <div id="myDropdown" class="dropdown-content">
+                                        <a href="#">Link 1</a>
+                                        <a href="#">Link 2</a>
+                                        <div>            
+                                            <!-- logged in user information -->
+                                            <?php if (isset($_SESSION['nickname'])) : ?>
+                                                <a href="index.php?logout='1'" style="color: red;">logout</a>
+                                            <?php endif ?>
+                                        </div>
+                                  </div>
+                            </div> 
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        
+            <div class="fotoMid" class="ui-widget" >  
+                <h1 id="titlehome"><em>Choose your destination</em></h1>
+                <form id="citySearch"  action="server.php" method ='POST'>
+                    <input type="text" class="autoC ui-autocomplete-input" placeholder="Insert here!" name="nomeCitta" id="nomeCitta" >
+                </form>
+            </div>
+            <div class="grid-item sideCont">   
                   <?php
                   //recupero la citta' dell'utente
                     $city = $dbalpha->prepare("SELECT citta FROM iscritto WHERE Nickname ='" .$_SESSION['nickname']."'");
@@ -56,8 +73,7 @@ if (isset($_GET['logout'])) {
                    //ricerco gli eventi
                   $events = $dbalpha->prepare("SELECT id,titolo,datainizio,organizzatore FROM evento WHERE organizzatore IN (SELECT nickname FROM gestore WHERE citta ='" . $citta['citta'] . "') LIMIT 5 ");
                       $events->execute();
-                      $result=$events->fetchAll(PDO::FETCH_ASSOC);
-                    
+                      $result=$events->fetchAll(PDO::FETCH_ASSOC);        
                       foreach($result as $row){
                           $idE=$row['id'];
                           $titolo=$row['titolo'];
@@ -93,30 +109,39 @@ if (isset($_GET['logout'])) {
                           print $riga;
                       }
                   ?></div>
-            
-        </div>
+        
+    </div> 
+<!--        chiudo il grid container-->
+        
+      
+         <script>
+            function myFunction() {
+                document.getElementById("myDropdown").classList.toggle("show");
+            }
+
+            // Close the dropdown menu if the user clicks outside of it
+            window.onclick = function(event) {
+                if (!event.target.matches('.dropbtn')) {
+                    var dropdowns = document.getElementsByClassName("dropdown-content");
+                    var i;
+                    for (i = 0; i < dropdowns.length; i++) {
+                      var openDropdown = dropdowns[i];
+                      if (openDropdown.classList.contains('show')) {
+                          openDropdown.classList.remove('show');
+                      }
+                    }
+                }
+            }
+                
+                $(document).ready(function() {
+                    //autocomplete
+                    $(".autoC").autocomplete({
+                        source: "server.php",
+                        minLength: 1
+                    });				
+                });
+        </script>
         
         
-        <div class="content">    
-<!--     php closing       -->
-            
-            <!-- notification message -->
-            <?php if (isset($_SESSION['success'])) : ?>
-                <div class="error success" >
-                    <h3>
-                        <?php
-                        echo $_SESSION['success'];
-                        unset($_SESSION['success']);
-                        ?>
-                    </h3>
-                </div>
-            <?php endif ?>
-
-            <!-- logged in user information -->
-            <?php if (isset($_SESSION['nickname'])) : ?>
-                <p> <a href="index.php?logout='1'" style="color: red;">logout</a> </p>
-            <?php endif ?>
-        </div>
-
     </body>
 </html>
